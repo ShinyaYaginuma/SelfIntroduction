0586ab95a98e3cca5a6d1f7b64d37a422e995120
Shinya Yaginuma <shinya.yaginuma.0507@gmail.com>
2021-05-21 09:43:59 +0900

    Update AppHeader.vue
    SVGタグの修正

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -89,8 +89,15 @@
               target="_blank"
               rel="noopener noreferrer"
               href="https://github.com/ShinyaYaginuma"
-              ><i class="bi bi-github"></i> GitHub</a
-            >
+              ><i class="bi bi-github"></i> GitHub<svg
+                style="width: 18px; height: 18px; padding: 3px"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"
+                /></svg
+            ></a>
           </v-list-item>
           <v-list-item>
             <a
@@ -98,8 +105,15 @@
               target="_blank"
               rel="noopener noreferrer"
               href="http://s-yaginu.hatenablog.com/"
-              ><i class="bi bi-journals"></i> hatena</a
-            >
+              ><i class="bi bi-journals"></i> hatena<svg
+                style="width: 18px; height: 18px; padding: 3px"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"
+                /></svg
+            ></a>
           </v-list-item>
           <v-list-item>
             <a
@@ -109,7 +123,15 @@
               rel="noopener noreferrer"
             >
               <i class="bi bi-twitter"></i> Contact
-            </a>
+              <svg
+                style="width: 18px; height: 18px; padding: 3px"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  fill="currentColor"
+                  d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z"
+                /></svg
+            ></a>
           </v-list-item>
         </v-list-item-group>
       </v-list>
