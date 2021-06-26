02754c48ab2b985bcff7cd6dd0dfcd47ac92f8e2
Shinya Yaginuma <shinya.yaginuma.0507@gmail.com>
2021-06-26 18:46:39 +0900

    editted home.vue
    test commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,8 @@
 <template>
   <v-container id="home">
-    <AboutMe msg="Welcome!!!!" />
+    <AboutMe />
     <Skills />
   </v-container>
-  <!-- <v-container id="home">
-    <v-row justify="center" align-content="center">
-      <v-col cols="12" lg="12" md="12" sm="12" align="center">
-        <img
-          class="rounded-circle"
-          alt="My Icon"
-          src="../assets/TwitterIcon.png"
-          width="300px"
-          height="auto"
-        />
-        <h5><em>I'm Shinya Yaginuma</em></h5>
-      </v-col>
-      <v-col cols="12" lg="12" md="12" sm="12" align="center">
-        <AboutMe msg="Welcome to My Page" />
-      </v-col>
-    </v-row>
-  </v-container> -->
 </template>
 
 <script>
